642a5cc830744ed92ab6912d3f9e2ed325ea0f7c
Cukierek <cukierek@programmer.net>
2017-12-17 10:55:52 +0100

    fixed application.properties, now you can use run parameters

--- src/main/java/Cms.java
+++ src/main/java/Cms.java
@@ -2,8 +2,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringApp {
+public class Cms {
     public static void main(String[] args) {
-        SpringApplication.run(SpringApp.class, args);
+        SpringApplication.run(Cms.class, args);
     }
 }
