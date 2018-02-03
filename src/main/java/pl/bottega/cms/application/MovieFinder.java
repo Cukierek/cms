9aba80313c329e6075c0f42f231388f444828437
Bartłomiej Olszówka <olszowka.bartlomiej@gmail.com>
2018-02-03 15:48:48 +0100

    (#5) added dtos and movie finder

---
+++ src/main/java/pl/bottega/cms/application/MovieFinder.java
@@ -0,0 +1,8 @@
+package pl.bottega.cms.application;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface MovieFinder {
+    List<MovieDto> getMovies(Long cinemaId, LocalDate date);
+}
